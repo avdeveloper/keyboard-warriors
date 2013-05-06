be9d947a6dbbe62d9357703bf5d75ef65a97a766
Albert Villaroman <avdeveloper@gmail.com>
2013-05-06 19:41:11 -0400

    added nodejs server

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require("express"),
+    atob = require("atob"),
+    app = module.exports = express();
+
+app.use(express.static("public"));
+app.use(express.bodyParser());
+app.engine(".html", require("ejs").__express);
+app.set("views", __dirname + "/public");
+app.set("view engine", "html");
+
+app.get("/", function (req, res) {
+  res.render("index");
+});
+
+
+if (!module.parent) {
+  var port = process.env.PORT || 8080;
+  app.listen(port);
+  console.log("Express app started on port %s", port);
+}
